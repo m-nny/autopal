d99ace9744040934bf6ff469e3517b15bbcfd563
Alibek Manabayev <alibek.manabayev@gmail.com>
2024-04-28 09:03:36 +0100

    admin_cli: extract tui to separate package

---
+++ pkg/admin/model.go
@@ -0,0 +1,110 @@
+package admin
+
+import (
+	"fmt"
+	"io"
+	"strings"
+
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+var _ list.Item = item("")
+
+type item string
+
+func (i item) FilterValue() string { return "" }
+
+var _ list.ItemDelegate = itemDelegate{}
+
+var (
+	itemStyle         = lipgloss.NewStyle().PaddingLeft(4)
+	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
+	quitTextStyle     = lipgloss.NewStyle().Margin(1, 0, 2, 4)
+)
+
+type itemDelegate struct{}
+
+func (d itemDelegate) Height() int                             { return 1 }
+func (d itemDelegate) Spacing() int                            { return 0 }
+func (d itemDelegate) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
+func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
+	i, ok := listItem.(item)
+	if !ok {
+		return
+	}
+
+	str := fmt.Sprintf("%d. %s", index+1, i)
+
+	fn := itemStyle.Render
+	if index == m.Index() {
+		fn = func(s ...string) string {
+			return selectedItemStyle.Render("> " + strings.Join(s, " "))
+		}
+	}
+
+	fmt.Fprint(w, fn(str))
+}
+
+var _ tea.Model = (*RootModel)(nil)
+
+type RootModel struct {
+	list     list.Model
+	choice   string
+	quitting bool
+}
+
+func NewModel() *RootModel {
+	items := []list.Item{
+		item("Ramen"),
+		item("Tomato Soup"),
+	}
+	l := list.New(items, itemDelegate{}, 20, 14)
+	l.Title = "What do you want for dinner?"
+	l.SetShowStatusBar(false)
+	l.SetFilteringEnabled(false)
+	l.Styles.Title = lipgloss.NewStyle().MarginLeft(2)
+	l.Styles.PaginationStyle = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
+	l.Styles.HelpStyle = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
+	return &RootModel{
+		list: l,
+	}
+}
+
+func (m *RootModel) Init() tea.Cmd {
+	return nil
+}
+
+func (m *RootModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.list.SetWidth(msg.Width)
+		return m, nil
+	case tea.KeyMsg:
+		switch keypress := msg.String(); keypress {
+		case "q", "ctrl+c":
+			m.quitting = true
+			return m, tea.Quit
+		case "enter":
+			i, ok := m.list.SelectedItem().(item)
+			if ok {
+				m.choice = string(i)
+			}
+			return m, tea.Quit
+		}
+	}
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
+}
+
+func (m *RootModel) View() string {
+	if m.choice != "" {
+		return quitTextStyle.Render(fmt.Sprintf("%s? Sounds good for me.", m.choice))
+	}
+	if m.quitting {
+		return quitTextStyle.Render("Not hungry? That's cool.")
+	}
+	return "\n" + m.list.View()
+}
